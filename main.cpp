565eba9d6ca7490781adc833976f55532b61626b
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2023-10-02 12:50:24 +0900

    addからminに変更

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
 ﻿#include<stdio.h>
 
 template <typename T>
-T add(T a, T b) {
+T Min(T a, T b) {
 	return a < b ? b : a;
 }
 template <>
-char add<char>(char a, char b) {
+char Min<char>(char a, char b) {
 	printf("数字以外は代入できません\n");
 	return '\0';
 }
 
 int main() {
 
-	printf("%d\n", add<int>(114, 51));
-	printf("%f\n", add<float>(114.0f, 51.4f));
-	printf("%lf\n", add<double>(114.0, 51.4));
-	printf("%c\n", add<char>(static_cast<char>('114'), static_cast <char>('514')));
+	printf("%d\n", Min<int>(114, 51));
+	printf("%f\n", Min<float>(114.0f, 51.4f));
+	printf("%lf\n", Min<double>(114.0, 51.4));
+	printf("%c\n", Min<char>(static_cast<char>('114'), static_cast <char>('514')));
 
 	return 0;
 }
