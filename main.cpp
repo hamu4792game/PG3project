58dc1e250eca9b3dc079719e777c6f13568154f0
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2023-10-16 11:25:33 +0900

    charの文字列をオーバーロードにし、ネストの回避

--- main.cpp
+++ main.cpp
@@ -4,10 +4,9 @@ template <typename T>
 T Min(T a, T b) {
 	return a < b ? b : a;
 }
-template <>
-char Min<char>(char a, char b) {
-	printf("数字以外は代入できません\n");
-	return '\0';
+char* Min(char a, char b) {
+	static char string[] = "数字以外は代入できません\n";
+	return string;
 }
 
 int main() {
@@ -15,7 +14,7 @@ int main() {
 	printf("%d\n", Min<int>(114, 51));
 	printf("%f\n", Min<float>(114.0f, 51.4f));
 	printf("%lf\n", Min<double>(114.0, 51.4));
-	printf("%c\n", Min<char>(static_cast<char>('114'), static_cast <char>('514')));
+	printf("%s\n", Min(static_cast<char>('114'), static_cast <char>('514')));
 
 	return 0;
 }
