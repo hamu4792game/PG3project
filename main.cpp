0cdace308d056284de38b1e935113be77cc3b088
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2024-01-15 11:44:23 +0900

    非同期の実装

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
-#include <stdio.h>
+#include <iostream>
+#include <thread>
+#include <string>
+
+void DrawThread(int num) {
+	std::cout << "thread " << num << std::endl;
+}
+
 
 int main() {
-	
+
+	int num = 1;
+
+	std::thread th1(DrawThread, num++);
+	th1.join();
+
+	std::thread th2(DrawThread, num++);
+	th2.join();
+
+	std::thread th3(DrawThread, num++);
+	th3.join();
+
 	return 0;
 }
+
