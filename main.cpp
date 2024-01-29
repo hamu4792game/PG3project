e6b0f43ca538010360556a199b7350ef9d1ed6a3
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2024-01-29 10:47:12 +0900

    copyとmoveの処理時間の違いを確認

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <chrono>
 
 int main() {
 	
+	std::string text(100000, 'a');
+
+	std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
+
+	std::string cText = text;
+
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+	auto time = end - now;
+
+	auto msec = std::chrono::duration_cast<std::chrono::microseconds>(time);
+	std::cout << msec << std::endl;
+
+
+	now = std::chrono::steady_clock::now();
+	
+	cText = std::move(text);
+	end = std::chrono::steady_clock::now();
+
+	time = end - now;
+	msec = std::chrono::duration_cast<std::chrono::microseconds>(time);
+	std::cout << msec << std::endl;
+
+
+
 	return 0;
 }
