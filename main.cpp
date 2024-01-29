df719525e03776d450ecd63c459dd5c3a0b66d0a
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2024-01-29 11:05:41 +0900

    出力文字を合わせた

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 int main() {
 	
+	std::cout << "100.000文字を移動とコピーで比較しました" << std::endl;
 	std::string text(100000, 'a');
 
 	std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
@@ -14,7 +15,7 @@ int main() {
 	auto time = end - now;
 
 	auto msec = std::chrono::duration_cast<std::chrono::microseconds>(time);
-	std::cout << msec << std::endl;
+	std::cout << "コピー : " << msec << std::endl;
 
 
 	now = std::chrono::steady_clock::now();
@@ -24,7 +25,7 @@ int main() {
 
 	time = end - now;
 	msec = std::chrono::duration_cast<std::chrono::microseconds>(time);
-	std::cout << msec << std::endl;
+	std::cout << "移動 : " << msec << std::endl;
 
 
 
